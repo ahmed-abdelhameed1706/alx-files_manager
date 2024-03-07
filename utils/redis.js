4e43495bc5a65e3c26c4b59cf3540dbbf2af404f
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-03-07 07:20:36 +0200

    1 bug left in redis file

---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+import { createClient } from "redis";
+import { promisify } from "util";
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on("error", (error) => {
+      console.log("Redis Client Error: ", error);
+    });
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setexAsync = promisify(this.client.setex).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return await this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    await this.setexAsync(key, duration, value);
+  }
+
+  async del(key) {
+    await this.delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
