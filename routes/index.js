60fd7568ea1a1ef23391ae4a5e4aad0a81c13e86
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-03-09 19:24:04 +0200

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-import express from "express";
 import { getStats, getStatus } from "../controllers/AppController";
+import { postNew } from "../controllers/UsersController";
+const express = require("express");
 
 const router = express.Router();
 
 router.get("/status", getStatus);
 router.get("/stats", getStats);
+
+router.post("/users", postNew);
 
 export default router;
