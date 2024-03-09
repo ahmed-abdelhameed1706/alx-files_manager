da45771c86f26f82af3b1da61a79dfecfb81b9d8
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-03-09 19:22:40 +0200

    testing

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
-import { getStats, getStatus } from '../controllers/AppController';
-
-const express = require('express');
+import express from "express";
+import { getStats, getStatus } from "../controllers/AppController";
 
 const router = express.Router();
 
-router.get('/status', getStatus);
-router.get('/stats', getStats);
+router.get("/status", getStatus);
+router.get("/stats", getStats);
 
 export default router;
