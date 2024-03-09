6e27105c27fd307d29cc04113e6fa8bf75653c8b
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-03-09 19:34:17 +0200

    routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
-import bodyParser from 'body-parser';
-import { getStats, getStatus } from '../controllers/AppController';
-import { postNew } from '../controllers/UsersController';
+import bodyParser from "body-parser";
+import { getStats, getStatus } from "../controllers/AppController";
+import { postNew } from "../controllers/UsersController";
 
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.use(bodyParser.json());
-router.get('/status', getStatus);
-router.get('/stats', getStats);
-
-router.post('/users', postNew);
+router.get("/status", getStatus);
+router.get("/stats", getStats);
 
 export default router;
