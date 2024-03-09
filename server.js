43b34b9db9ba1456a1722a014d0bf329de8b97b1
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-03-09 19:18:52 +0200

    server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-import router from './routes';
-
-const express = require('express');
+import express from "express";
+import router from "./routes";
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running in localhost:${port}`);
 });
+
+export default app;
