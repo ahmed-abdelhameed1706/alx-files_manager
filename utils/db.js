dce8b6fe7a764711a4a44c434dfdc8f2224b7c86
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-03-13 06:50:19 +0200

    fixing lint

--- utils/db.js
+++ utils/db.js
@@ -1,17 +1,17 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || "localhost";
+    const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || "files_manager";
+    const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}`;
 
     MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
       if (!err) {
         this.db = client.db(database);
-        this.users = this.db.collection("users");
-        this.files = this.db.collection("files");
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
       } else {
         this.db = null;
       }
