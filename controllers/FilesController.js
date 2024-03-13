7df86b1a8a92d725671d3d0576feb0d8cecaad12
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-03-14 00:34:44 +0200

    task 6

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -5,7 +5,7 @@ import path from 'path';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-const postUpload = async (req, res) => {
+export const postUpload = async (req, res) => {
   const token = req.header('X-Token');
   const key = `auth_${token}`;
   const userId = await redisClient.get(key);
@@ -86,4 +86,63 @@ const postUpload = async (req, res) => {
   });
 };
 
-export default postUpload;
+export const getShow = async (req, res) => {
+  const token = req.header('X-Token');
+  if (!token) {
+    return res.status(401).send({ error: 'Unauthorized' });
+  }
+  const key = `auth_${token}`;
+  const userId = await redisClient.get(key);
+  if (!userId) {
+    return res.status(401).send({ error: 'Unauthorized' });
+  }
+  const user = await dbClient.users.findOne({ _id: ObjectId(userId) });
+  if (!user) {
+    return res.status(401).send({ error: 'Unauthorized' });
+  }
+  const fileId = req.params.id;
+
+  const file = await dbClient.files.findOne({ _id: ObjectId(fileId) });
+
+  if (!file) {
+    return res.status(404).send({ error: 'Not found' });
+  }
+
+  if (file.userId.toString() !== userId) {
+    return res.status(404).send({ error: 'Not found' });
+  }
+
+  return res.status(200).send({
+    id: file._id,
+    userId: file.userId,
+    name: file.name,
+    type: file.type,
+    isPublic: file.isPublic,
+    parentId: file.parentId,
+  });
+};
+
+export const getIndex = async (req, res) => {
+  const token = req.header('X-Token');
+  if (!token) {
+    return res.status(401).send({ error: 'Unauthorized' });
+  }
+  const key = `auth_${token}`;
+  const userId = await redisClient.get(key);
+  if (!userId) {
+    return res.status(401).send({ error: 'Unauthorized' });
+  }
+  const user = await dbClient.users.findOne({ _id: ObjectId(userId) });
+  if (!user) {
+    return res.status(401).send({ error: 'Unauthorized' });
+  }
+  const parentId = req.query.parentId || 0;
+  const files = await dbClient.files
+    .aggregate([
+      { $match: { userId: ObjectId(userId), parentId: ObjectId(parentId) } },
+      { $limit: 20 },
+    ])
+    .toArray();
+  return res.status(200).send(files);
+};
+// curl -XGET 0.0.0.0:5000/files -H "X-Token: 25c94fb2-81e7-44ae-8e20-fe2eae0c5fe2" ; echo ""
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import { getStats, getStatus } from '../controllers/AppController';
 import { postNew, getMe } from '../controllers/UsersController';
 import { getConnect, getDisconnect } from '../controllers/AuthController';
-import postUpload from '../controllers/FilesController';
+import { postUpload, getShow, getIndex } from '../controllers/FilesController';
 
 const express = require('express');
 
@@ -20,5 +20,7 @@ router.get('/disconnect', getDisconnect);
 router.get('/users/me', getMe);
 
 router.post('/files', postUpload);
+router.get('/files/:id', getShow);
+router.get('/files', getIndex);
 
 export default router;
