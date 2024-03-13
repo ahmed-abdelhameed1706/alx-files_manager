915143dd85e0a3632962aac99e513b95b812568a
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-03-13 16:45:51 +0200

    task 5

---
+++ controllers/FilesController.js
@@ -0,0 +1,89 @@
+import { ObjectId } from 'mongodb';
+import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+const postUpload = async (req, res) => {
+  const token = req.header('X-Token');
+  const key = `auth_${token}`;
+  const userId = await redisClient.get(key);
+  if (!userId) {
+    return res.status(401).send('Unauthorized');
+  }
+  const user = await dbClient.users.findOne({ _id: ObjectId(userId) });
+
+  if (!user) {
+    return res.status(401).send('Unauthorized');
+  }
+
+  const {
+    name, type, parentId, isPublic, data,
+  } = req.body;
+
+  if (!name) {
+    return res.status(400).send('Missing name');
+  }
+
+  if (!type || (type !== 'folder' && type !== 'file' && type !== 'image')) {
+    return res.status(400).send('Missing type');
+  }
+
+  if (type !== 'folder' && !data) {
+    return res.status(400).send('Missing data');
+  }
+
+  if (parentId) {
+    const parent = await dbClient.files.findOne({ _id: ObjectId(parentId) });
+    if (!parent) {
+      return res.status(400).send('Parent not found');
+    }
+
+    if (parent.type !== 'folder') {
+      return res.status(400).send('Parent is not a folder');
+    }
+  }
+  if (type === 'folder') {
+    const newFolder = await dbClient.files.insertOne({
+      userId: ObjectId(userId),
+      name,
+      type,
+      isPublic,
+      parentId: parentId || 0,
+    });
+    return res.status(201).send({
+      id: newFolder.insertedId,
+      userId: ObjectId(userId),
+      name,
+      type,
+      isPublic,
+      parentId: parentId || 0,
+    });
+  }
+  const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
+  }
+  const filePath = path.join(folderPath, `${uuidv4()}`);
+  const fileData = Buffer.from(data, 'base64');
+  fs.writeFileSync(filePath, fileData);
+  const newFile = await dbClient.files.insertOne({
+    userId: ObjectId(userId),
+    name,
+    type,
+    isPublic,
+    parentId: parentId || 0,
+    localPath: filePath,
+  });
+  return res.status(201).send({
+    id: newFile.insertedId,
+    userId: ObjectId(userId),
+    name,
+    type,
+    isPublic,
+    parentId: parentId || 0,
+  });
+};
+
+export default postUpload;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { getStats, getStatus } from '../controllers/AppController';
 import { postNew, getMe } from '../controllers/UsersController';
 import { getConnect, getDisconnect } from '../controllers/AuthController';
+import postUpload from '../controllers/FilesController';
 
 const express = require('express');
 
@@ -17,5 +18,7 @@ router.post('/users', postNew);
 router.get('/connect', getConnect);
 router.get('/disconnect', getDisconnect);
 router.get('/users/me', getMe);
+
+router.post('/files', postUpload);
 
 export default router;
