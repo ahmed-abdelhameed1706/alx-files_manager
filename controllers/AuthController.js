b43c0fb604b1f73d9a1288d416361c44a54a006e
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-03-13 21:11:19 +0200

    fixing a minor bug

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,42 +1,41 @@
-import { v4 } from 'uuid';
-import sha1 from 'sha1';
-import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
+import { v4 } from "uuid";
+import sha1 from "sha1";
+import redisClient from "../utils/redis";
+import dbClient from "../utils/db";
 
-const decrypt64 = (str) => Buffer.from(str, 'base64').toString('utf-8');
+const decrypt64 = (str) => Buffer.from(str, "base64").toString("utf-8");
 
 export const getConnect = async (req, res) => {
-  const encrypted = req.headers.authorization.split(' ')[1];
+  const encrypted = req.headers.authorization.split(" ")[1];
 
   if (!encrypted) {
-    return res.status(401).send('Unauthorized');
+    return res.status(401).send("Unauthorized");
   }
 
   const decrypted = decrypt64(encrypted);
-  const email = decrypted.split(':')[0];
-  const password = decrypted.split(':')[1];
+  const email = decrypted.split(":")[0];
+  const password = decrypted.split(":")[1];
 
   const user = await dbClient.users.findOne({ email });
 
   if (!user || user.password !== sha1(password)) {
-    return res.status(401).send('Unauthorized');
+    return res.status(401).send("Unauthorized");
   }
 
   const token = v4();
   const key = `auth_${token}`;
   redisClient.set(key, user._id.toString(), 86400);
-  console.log(`Welcome to the dashboard ${user.email}`);
   return res.status(200).send({ token });
 };
 
 export const getDisconnect = async (req, res) => {
-  const token = req.header('X-Token');
+  const token = req.header("X-Token");
   const key = `auth_${token}`;
 
   const userId = await redisClient.get(key);
 
   if (!userId) {
-    return res.status(401).send({ error: 'Unauthorized' });
+    return res.status(401).send({ error: "Unauthorized" });
   }
 
   await redisClient.del(key);
